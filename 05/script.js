3de364e2cc713ddf38b3c7befa22d690b095ae2f
xlArtemlx <59087414+xlArtemlx@users.noreply.github.com>
2020-01-03 17:41:33 +0200

    aside 2

--- 05/script.js
+++ 05/script.js
@@ -1,12 +1,9 @@
 var day = "thursday"
 var result = day === "saturday" || day === "sunday" ? console.log ("Спать!") : console.log ("Иди работай!!")
-
-
-
 var day = "thursday"
-	if (day === "saturday" || day === "sunday") {
-	      console.log("Спать!!!")
-	}
-	else { 
-	      console.log("на работу!!!")
-	}
+ if (day === "saturday" || day === "sunday") {
+  console.log("Спать!!!")
+ }
+ else { 
+  console.log("на работу!!!")
+ }
--- 08/script.js
+++ 08/script.js
@@ -1,3 +1,2 @@
 var username = "Artem"
-
-	console.log("Happy new year", username)
+ console.log("Happy new year", username)
