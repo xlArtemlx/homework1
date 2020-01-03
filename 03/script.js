71816430b96b9f8fc3974de9246c788b81f68ff5
xlArtemlx <59087414+xlArtemlx@users.noreply.github.com>
2020-01-03 20:25:38 +0200

    good aside

--- 03/script.js
+++ 03/script.js
@@ -1,12 +1,10 @@
 var x = 5
 var y = 6
 var z = 7
- if (x > y) {
-  console.log(y)
-  }
- else if (y > z)  {
-  console.log(z) 
-  }
- else {
-  console.log(x)
-  }
+if (x > y) {
+    console.log(y)
+} else if (y > z) {
+    console.log(z)
+} else {
+    console.log(x)
+}
